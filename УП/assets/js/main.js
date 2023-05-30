9a398a62f89074d79f053390935a5efa3fbead33
Arc3228 <105052275+Arc3228@users.noreply.github.com>
2023-05-30 21:47:31 +0300

    Add files via upload

---
+++ УП/assets/js/main.js
@@ -0,0 +1,56 @@
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu_button", ".burger-menu_lines");
+  let links = menu.find(".burger-menu_link");
+  let overlay = menu.find(".burger-menu_overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+
+    if (menu.hasClass("burger-menu_active")) {
+      $("body").css("overlow", "hidden");
+    } else {
+      $("body").css("overlow", "visible");
+    }
+  }
+}
+
+burgerMenu(".burger-menu");
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
